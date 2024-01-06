569e46bab90f68dfb57588f6c7df65e15b23dcf7
20213846 <n5lp97@gmail.com>
2024-01-06 17:54:39 +0900

    인터페이스 수정

--- src/Interface/SensorNode.ts
+++ src/Interface/SensorNode.ts
@@ -3,8 +3,7 @@ export default interface SensorNode {
     name: string;
     temperature: number;
     humidity: number;
-    battery: number;
     fireDetected: boolean;
-    movementDetected: boolean;
+    movementDetectedTime?: number;
     checkResult?: boolean; // 센서 동작 점검 결과
 }
